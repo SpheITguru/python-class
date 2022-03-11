9ecef9263216a8b11079f8751cbecffc7deff599
Siphephelo <sphe.mlu@gmail.com>
2022-03-11 17:46:24 +0200

    Create a hiding data field

--- TestCircle.py
+++ TestCircle.py
@@ -4,9 +4,10 @@ def main():
     circle1 = Circle()
     #print("The area of the circle of radius ",\
     #    circle1.radius, " is ", circle1.getArea())
-    circle2 = Circle(25)
+    circle2 = Circle(100)
     #circle2.radius = 100
-    print("Circle 2 radius is: ", circle2.radius)
+    #print("Circle 2 radius is: ", circle2.getRadius())
+    print("The Area of Radius: ",circle2.getRadius(), " is :",round(circle2.getArea(),3))
 
 if __name__ == '__main__':
     main()
